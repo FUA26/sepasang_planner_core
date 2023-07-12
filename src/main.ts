fef6c85d56db00a042a97e2b471ebae4169b5e56
Fatih <Fatihulil@gmail.com>
2023-07-12 16:26:10 +0700

    fix(Base): Add and Fixing Cros

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService) as ConfigService<AllConfigType>;
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.enableVersioning({
     type: VersioningType.URI,
   });
